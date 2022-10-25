2175999821b27f4c9e020001ed6571f34636cd49
OJESH <OJESH@DESKTOP-EUI8GBJ>
2022-10-25 17:38:18 +0530

    First Push

---
+++ EPS.Assignments/src/main/java/genericUtilities/IConstants.java
@@ -0,0 +1,12 @@
+package genericUtilities;
+
+public interface IConstants {
+
+	
+	String EXCELPATH ="../EPS.Assignments/src/main/resources/testData/HouseRentalTestData(Sprint-2).xlsx";
+	String propertyFilePath = "./RentalHouseData/rentalHouseData1.properties";
+	int Implicitly_TimeOut = 10;
+	int Explicitly_TimeOut = 10;
+	
+	
+}
